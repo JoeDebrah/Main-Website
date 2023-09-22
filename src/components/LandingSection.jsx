5975021008f255de27065feb0e5c722b8aa14676
JoeDeb <113562378+JoeDebrah@users.noreply.github.com>
2023-09-22 13:02:00 -0400

    Update LandingSection.jsx

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -9,7 +9,7 @@ function LandingSection() {
             <Row className='mt-5 mb-5 pt-5 pb-5'>
 
                 <Col xs={6} md={4} className='mx-auto mb-5 mt-5'>
-                    <Image className='mx-auto d-block mb-4' width={150} height={150} src="https://media.licdn.com/dms/image/D4E35AQHv-FHuER5fJA/profile-framedphoto-shrink_400_400/0/1693350769983?e=1695308400&v=beta&t=9b-1fhQFWDOoy7zlTf69BXTdQioskJG-LsU4IIBxKbw" roundedCircle />
+                    <Image className='mx-auto d-block mb-4' width={150} height={150} src="https://media.licdn.com/dms/image/D4E35AQHv-FHuER5fJA/profile-framedphoto-shrink_400_400/0/1693350769647?e=1696006800&v=beta&t=NTFzLnp18B_AGH2M5inkgUMQqw52TLRsSZdTob5sNBE" roundedCircle />
                     <div className='text-center text-dark'>
                         <h5>Hello there, I'm Joe</h5>
                         <h2>A Junior Front-End Developer Specialized In React</h2>
